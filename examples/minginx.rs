6601797f94408c9395e01d705f14128248bf5d68
Vincent_Li <q315744068@gmail.com>
2024-12-17 23:31:22 +0800

    UPDATE: init chat app

--- examples/minginx.rs
+++ examples/minginx.rs
@@ -3,24 +3,24 @@ use std::sync::Arc;
 use anyhow::Result;
 use derive_more::derive::Debug;
 use serde::{Deserialize, Serialize};
-use tokio::{io, net::{TcpListener, TcpStream}};
+use tokio::{
+    io,
+    net::{TcpListener, TcpStream},
+};
 use tracing::{info, level_filters::LevelFilter, warn};
 use tracing_subscriber::{fmt::Layer, layer::SubscriberExt, util::SubscriberInitExt, Layer as _};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Config {
-  listen_addr: String,
-  upstream_addr: String,
+    listen_addr: String,
+    upstream_addr: String,
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    
-    let layer = Layer::new()
-      .pretty()
-      .with_filter(LevelFilter::INFO);
+    let layer = Layer::new().pretty().with_filter(LevelFilter::INFO);
     tracing_subscriber::registry().with(layer).init();
-    
+
     let config = resolve_config();
     let config = Arc::new(config);
     info!("Upstream address: {}", config.upstream_addr);
@@ -28,14 +28,14 @@ async fn main() -> Result<()> {
 
     let listener = TcpListener::bind(&config.listen_addr).await?;
     loop {
-      let (client, addr) = listener.accept().await?;
-      let cloned_config = Arc::clone(&config);
-      info!("New connection from {}", addr);
-      tokio::spawn(async move {
-        let upstream = TcpStream::connect(&cloned_config.upstream_addr).await?;
-        proxy(client, upstream).await?;
-        Ok::<(), anyhow::Error>(())
-      });
+        let (client, addr) = listener.accept().await?;
+        let cloned_config = Arc::clone(&config);
+        info!("New connection from {}", addr);
+        tokio::spawn(async move {
+            let upstream = TcpStream::connect(&cloned_config.upstream_addr).await?;
+            proxy(client, upstream).await?;
+            Ok::<(), anyhow::Error>(())
+        });
     }
 
     #[allow(unreachable_code)]
@@ -51,14 +51,14 @@ async fn proxy(mut client: TcpStream, mut upstream: TcpStream) -> Result<()> {
     let upstream_to_client = io::copy(&mut upstream_reader, &mut client_writer);
 
     if let Err(e) = tokio::try_join!(client_to_upstream, upstream_to_client) {
-      warn!("Error: {}", e);
+        warn!("Error: {}", e);
     };
     Ok(())
 }
 
 fn resolve_config() -> Config {
-  Config {
-    listen_addr: "127.0.0.1:3000".to_string(),
-    upstream_addr: "127.0.0.1:3001".to_string(),
-  }
+    Config {
+        listen_addr: "127.0.0.1:3000".to_string(),
+        upstream_addr: "127.0.0.1:3001".to_string(),
+    }
 }
--- examples/tokio1.rs
+++ examples/tokio1.rs
@@ -6,11 +6,11 @@ fn main() {
     let handle = thread::spawn(|| {
         let rt = Builder::new_current_thread().enable_all().build().unwrap();
 
-		rt.spawn(async {
-			println!("Future 1!");
-			let content = fs::read_to_string("Cargo.toml").await.unwrap();
+        rt.spawn(async {
+            println!("Future 1!");
+            let content = fs::read_to_string("Cargo.toml").await.unwrap();
             println!("content lent {}", content.len());
-		});
+        });
 
         rt.spawn(async {
             println!("Future 2!");
